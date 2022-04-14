b0737636b8809ea9163e410ea58bbafa9b43a3be
foxdev <foxdev@pdx.edu>
2022-04-13 18:06:48 -0700

    test moving average filter and reading wave

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
-use std::f32::consts::PI;
 use std::env;
+use std::f32::consts::PI;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    // let args: Vec<String> = env::args().collect();
+    // println!("{:?}", args);
+    let filename = env::args().nth(1).expect("no filename provided");
+    let srate = env::args().nth(2).expect("no sample rate provided");
+    let srate = srate.trim().parse::<u32>().unwrap();
+    let mut reader = hound::WavReader::open(&filename).unwrap();
+    let samples: Vec<i16> = reader.samples().map(|s| s.unwrap()).collect();
 
     let spec = hound::WavSpec {
         channels: 1,
-        sample_rate: 48000,
+        sample_rate: srate,
         bits_per_sample: 16,
         sample_format: hound::SampleFormat::Int,
     };
@@ -15,6 +20,18 @@ fn main() {
     let frequency = 440.0;
     let duration: u32 = spec.sample_rate; // one second
     output_basic_sine(spec, frequency, duration);
+    let newsamples = filter(samples);
+    for sample in newsamples {
+        println!("{:?}", sample);
+    }
+    
+}
+
+fn filter(mut samples: Vec<i16>) -> Vec<i16> {
+    for sample in &mut samples {
+        *sample /= 2;
+    }
+    samples
 }
 
 fn output_basic_sine(spec: hound::WavSpec, freq: f32, duration: u32) {
