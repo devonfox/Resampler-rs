22e062a47f6d31ee842b369a62b6ac9536620dfa
foxdev <foxdev@pdx.edu>
2022-04-15 17:28:43 -0700

    commented a few functions

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,13 @@ use std::fs::File;
 use std::io::{prelude::*, BufReader};
 
 fn main() {
-    println!("Args: {}", env::args().count() - 1);
+    // Takes first argument as a filename to a wav file to resample to half the rate
 
     let filename = env::args().nth(1).expect("no filename provided");
     let mut reader = hound::WavReader::open(&filename).unwrap();
     let samples: Vec<i16> = reader.samples().map(|s| s.unwrap()).collect();
     let oldspec: hound::WavSpec = reader.spec();
+    assert_eq!(oldspec.channels, 1, "Stereo file not accepted.");
 
     let newspec = hound::WavSpec {
         channels: oldspec.channels,
@@ -22,11 +23,14 @@ fn main() {
     let duration = reader.duration() / oldspec.sample_rate;
     println!("Source File: '{}'", filename);
     println!("Duration: {} second(s)", duration);
-    println!("Wav Sample Rate: {}", wav_samprate);
+    println!("Wav Sample Rate: {} sps", wav_samprate);
     resample(oldspec, newspec, samples, filename);
 }
 
-fn filter(mut samples: Vec<i16>) -> Vec<i16> {
+/// FIR filter function utilizes an external file of filter coefficients
+/// provided by Bart Massey
+fn filter(samples: Vec<i16>) -> Vec<i16> {
+    // Read from coeffs.txt provided by Bart Massey
     let file = File::open("coeffs.txt").expect("coeffs.txt not found");
     let parser = BufReader::new(file);
     let mut coeffs: Vec<f64> = Vec::new();
@@ -36,14 +40,13 @@ fn filter(mut samples: Vec<i16>) -> Vec<i16> {
             .trim()
             .parse::<f64>()
             .unwrap();
-        // println!("{coeff}");
         coeffs.push(coeff);
     }
-    // println!("Size of Coeffs vec: {}", coeffs.len());
+
     let ilength = samples.len();
     let jlength = coeffs.len();
     let mut filtered: Vec<i16> = Vec::new();
-    for i in 92..ilength {
+    for i in jlength..ilength {
         let mut add: f64 = 0.0;
         for j in 0..jlength - 1 {
             let stuff = coeffs[j] * samples[i - j] as f64;
@@ -51,23 +54,25 @@ fn filter(mut samples: Vec<i16>) -> Vec<i16> {
         }
         filtered.push(add as i16);
     }
-    for i in 0..91 {
+    for i in 0..jlength {
         filtered.insert(i, 0);
     }
     filtered
 }
 
+/// Resample function takes vector of samples, calls FIR filter function on 'samples'
+/// and outputs every other sample to a wave file
 fn resample(oldspec: WavSpec, newspec: WavSpec, samples: Vec<i16>, filename: String) {
     let mut rfilename = filename;
     rfilename.insert(0, 'r');
     let mut rsamp_write = hound::WavWriter::create(&rfilename, newspec).unwrap();
     let count = samples.len() / 2;
     let resample = filter(samples);
-    println!("\nInput sample rate: {}", oldspec.sample_rate);
-    println!("Output sample rate: {}\n", newspec.sample_rate);
+
+    println!("\nInput sample rate: {} sps", oldspec.sample_rate);
+    println!("Output sample rate: {} sps\n", newspec.sample_rate);
     for i in (0..resample.len()).step_by(2) {
         rsamp_write.write_sample(resample[i]).unwrap();
-        // println!("{}", i);
     }
 
     println!("Created File: '{}'", rfilename);
@@ -75,6 +80,6 @@ fn resample(oldspec: WavSpec, newspec: WavSpec, samples: Vec<i16>, filename: Str
         "Duration: {} second(s)",
         (count as u32) / newspec.sample_rate
     );
-    println!("Wav Sample Rate: {}\n", newspec.sample_rate);
+    println!("Wav Sample Rate: {} sps\n", newspec.sample_rate);
     rsamp_write.finalize().unwrap();
 }
